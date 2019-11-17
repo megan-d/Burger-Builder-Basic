6c601a3e881da3ed4fe9291f7a57d9cab928242a
Megan D <45672063+megan-d@users.noreply.github.com>
2019-11-17 15:57:10 -0700

    Set up burger ingredients to display appropriate number of ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,7 +5,7 @@ import classes from './Burger.module.css';
 
 const burger = (props) => {
     //transform ingredients object prop into array of the values of the ingredients (properties)
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             //array as many times as there are number of that ingredients in state to give us length
             return [...Array(props.ingredients[igKey])]
@@ -13,7 +13,16 @@ const burger = (props) => {
                 //map object into array of ingredients. the length now allows us to display that ingredient the number of times specified in state.
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
-        });
+        })
+        //flatten array and create one array
+        .reduce((arr, el) => {
+            //take given element which we're looping and add it to array
+            return arr.concat(el);
+        }, []);
+        //if this new array length is 0 (empty), then display message to add ingredients
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients!</p>
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>
